39a3c6c2f0fe432b48974e52a06c8711fa24b992
melisnv <melisnurverir@gmail.com>
2023-05-21 14:35:12 +0200

    small changes

--- buildGraph.py
+++ buildGraph.py
@@ -35,18 +35,28 @@ for entry in merged_data:
         g_all_tweets.add((topic_uri, RDFS.label, Literal(topic_uri)))
         g_all_tweets.add((topic_uri, URIRef('http://example.com/isAbout'), tweet_uri))
 
-    # Process frame-related results
+    # processing frame-related results
     hasFrame_results = results['hasFrame']
     closeMatch_results = results['closeMatch']
 
     for result in hasFrame_results:
         frame_uri = rdflib.URIRef(result['frame']['value'])
-        frameName = result['related']['value'].split("/")[-1]  # Extract the frame name from the URL
+        frameName = result['frame']['value'].split("/")[-1]  # Extract the frame name from the URL
 
-        # Add frame triples
-        g_all_tweets.add((frame_uri, RDF.type, FRAME.Frame))
+        relatedFrame_uri = rdflib.URIRef(result['related']['value'])
+        relatedFrameName = result['related']['value'].split("/")[-1]  # Extract the frame name from the URL
+
+        # adding frame triples
+        g_all_tweets.add((frame_uri, RDF.type, URIRef('http://example.com/Frame')))
         g_all_tweets.add((frame_uri, RDFS.label, Literal(frameName)))
+
+        # adding hasFrameRelation
+        g_all_tweets.add((relatedFrame_uri, RDF.type, URIRef('http://example.com/SymmetricProperty')))
+        g_all_tweets.add((relatedFrame_uri, RDFS.label, Literal(relatedFrameName)))
+
         g_all_tweets.add((tweet_uri, URIRef('http://example.com/hasFrame'), frame_uri))
+        g_all_tweets.add((frame_uri, URIRef('http://example.com/hasFrameRelation'), relatedFrame_uri))
+        g_all_tweets.add((relatedFrame_uri, URIRef('http://example.com/isRelated'), frame_uri))
 
     for result in closeMatch_results:
         matchedFrames_uri = rdflib.URIRef(result['matchedFrames']['value'])
@@ -60,7 +70,7 @@ for entry in merged_data:
         g_all_tweets.add((matchedFrames_uri, URIRef('http://example.com/relatesTo'), frame_uri))
 
 # Serialize the RDF graph and save it to a file
-with open("data/20thMay.ttl", 'wb') as f:
+with open("graphs/deneme.ttl", 'wb') as f:
     f.write(g_all_tweets.serialize(format="turtle").encode())
 
-print(f"RDF graph saved to 20thMay.ttl file.")
+print(f"RDF graph saved to deneme.ttl file.")
