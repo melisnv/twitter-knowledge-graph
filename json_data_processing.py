9964949f8808b05ef538910f61d6f03672e97649
melisnv <melisnurverir@gmail.com>
2023-08-28 14:41:54 +0200

    merging twitter json files

--- json_data_processing.py
+++ json_data_processing.py
@@ -2,22 +2,28 @@ import pandas as pd
 import json
 import os
 
-# defining the directory where the JSON files are located
-json_dir = 'data/'
 
-# creating an empty list to store the dataframes
-df_list = []
+def process_json_data(json_dir):
+    '''
+    This function creates a dataframe from JSON files
+    :param json_dir: file location of the JSON files
+    :return: a dataframe created by merging JSON files
+    '''
 
-for filename in os.listdir(json_dir):
-    # checking if the file is a JSON file
-    if filename.endswith('.json'):
-        # loading the JSON data
-        with open(os.path.join(json_dir, filename), 'r', encoding='ISO-8859-1') as f:
-            data = json.load(f)
-        # converting the dictionary to a DataFrame and append it to the list
-        df_list.append(pd.DataFrame.from_dict(data))
+    # creating an empty list to store the dataframes
+    df_list = []
 
-# concatenating all dataframes in the list into one dataframe
-data = pd.concat(df_list, ignore_index=True)
-# saving the DataFrame as a CSV file
-data.to_csv('data/twitter_data.csv', index=False)
+    for filename in os.listdir(json_dir):
+        # checking if the file is a JSON file
+        if filename.endswith('.json'):
+            # loading the JSON data
+            with open(os.path.join(json_dir, filename), 'r', encoding='ISO-8859-1') as f:
+                data = json.load(f)
+            # converting the dictionary to a DataFrame and append it to the list
+            df_list.append(pd.DataFrame.from_dict(data))
+
+    # concatenating all dataframes in the list into one dataframe
+    data = pd.concat(df_list, ignore_index=True)
+
+    return data
+
